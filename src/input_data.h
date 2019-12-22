7eb358633b385726c9ae65dd0ae9e0ef623dd68d
Alexander Petrosyan <modernpacifist@gmail.com>
2019-12-22 16:03:28 +0300

    some newe changes in pattern

--- src/input_data.h
+++ src/input_data.h
@@ -4,22 +4,6 @@
 #include <string>
 
 
-std::string key_input() {
-	std::cout << "Please input your key" << std::endl;
-	std::string imposed_key;
-    getline(std::cin, imposed_key); 
-  	return imposed_key;
-}
-
-std::string phrase_input() {
-	std::cout << "Input your phrase to encrypt/decrypt" << std::endl;
-	std::string imposed_phrase;
-    getline(std::cin, imposed_phrase); 
-  	return imposed_phrase;
-}
-
-
-
 std::string data_input(std::string phrase) {
 	std::cout << phrase << std::endl;
 	std::string imposed_phrase;
