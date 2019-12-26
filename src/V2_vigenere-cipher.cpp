b1af5ad516ca804eb30bf4757a7595a45848a2f1
Alexander Petrosyan <modernpacifist@gmail.com>
2019-12-26 08:51:35 +0300

    second mode is working

--- src/V2_vigenere-cipher.cpp
+++ src/V2_vigenere-cipher.cpp
@@ -34,72 +34,13 @@ int main () {
         
         std::string imposed_message = data_input("Input your message:"); 
 
+        decrypting_algorithm(imposed_key, imposed_message, *english_language_ptr);
 
-   //     std::vector<char> message_to_decrypt;
-   //     message_to_decrypt.reserve(0);
-   //     std::cout << "Please insert your phrase, you want to encrypt use '.' at the end to stop the process"
-   //     << std::endl;
-   //     char letter_for_message_to_decrypt;
-   //     while (std::cin >> letter_for_message_to_decrypt) {
-   //         if (letter_for_message_to_decrypt != '.') {
-   //             message_to_decrypt.push_back(letter_for_message_to_decrypt);
-   //         } else {
-   //             break;
-   //         }
-   //     }
-   //     message_to_decrypt.shrink_to_fit();
+        return EXIT_SUCCESS;
 
-   //     std::vector<char> decrypted_message;
-   //     decrypted_message.reserve(0);
-
-   //     char letter_for_decrypted_message;
-   //     int index(0), index1(0);
-
-   //     for (int i = 0; i < message_to_decrypt.size(); i++) {
-   //         for (int j = 0; j < english_language.size(); j++) {
-   //             if (message_to_decrypt[i] == english_language[j]) {
-   //                 for (int k(index); k < d_keyWord.size();) {
-   //                     for (int l = 0; l < rotated_english_language.size(); l++) {
-   //                         if (d_keyWord[k] == rotated_english_language[l]) {
-   //                             std::rotate(rotated_english_language.begin(),
-   //                                     rotated_english_language.begin()+l,
-   //                                     rotated_english_language.end());
-   //                             for (int m(index1); m < message_to_decrypt.size();) {
-   //                                 for (int n = 0; n < rotated_english_language.size(); ++n) {
-   //                                     if (message_to_decrypt[m] == rotated_english_language[n]) {
-   //                                         letter_for_decrypted_message = english_language[n];
-   //                                         decrypted_message.push_back(letter_for_decrypted_message);
-   //                                     }
-   //                                 }
-   //                                 if (index1 == (message_to_decrypt.size()-1)) {
-   //                                     index1 = 0;
-   //                                 } else {
-   //                                     index1++;
-   //                                 }
-   //                                 break;
-   //                             }
-   //                             rotated_english_language = english_language;
-   //                         }
-   //                     }
-   //                     if (index == (d_keyWord.size()-1)) {
-   //                         index = 0;
-   //                     } else {
-   //                         index++;
-   //                     }
-   //                     break;
-   //                 }
-   //             }
-   //         }
-   //     }
-   //     decrypted_message.shrink_to_fit();
-
-   //     std::cout << "Your decrypted phrase is the following line: ";
-   //     for (size_t i(0); i < decrypted_message.size(); i++) {
-   //         std::cout << decrypted_message[i];
-   //     }
-   //     return EXIT_SUCCESS;
     } else {
     std::cout << "Mistake in choosing mode, please try again" << std::endl;
         EXIT_FAILURE;
     }
+
 }
--- src/decrypt_data.h
+++ src/decrypt_data.h
@@ -1,1 +1,63 @@
-//first commit
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+
+void decrypting_algorithm(std::string imposed_key, std::string imposed_message, 
+        std::vector<char> english_language) {
+
+    std::vector<char> rotated_english_language = english_language;
+
+    std::vector<char> decrypted_message;
+    decrypted_message.reserve(0);
+
+    char letter_for_decrypted_message;
+    int index(0), index1(0);
+
+    for (int i = 0; i < imposed_message.size(); i++) {
+        for (int j = 0; j < english_language.size(); j++) {
+            if (imposed_message[i] == english_language[j]) {
+                for (int k(index); k < imposed_key.size();) {
+                    for (int l = 0; l < rotated_english_language.size(); l++) {
+                        if (imposed_key[k] == rotated_english_language[l]) {
+                            std::rotate(rotated_english_language.begin(),
+                                    rotated_english_language.begin()+l,
+                                    rotated_english_language.end());
+                            for (int m(index1); m < imposed_message.size();) {
+                                for (int n = 0; n < rotated_english_language.size(); ++n) {
+                                    if (imposed_message[m] == rotated_english_language[n]) {
+                                        letter_for_decrypted_message = english_language[n];
+                                        decrypted_message.push_back(letter_for_decrypted_message);
+                                    }
+                                }
+                                if (index1 == (imposed_message.size()-1)) {
+                                    index1 = 0;
+                                } else {
+                                    index1++;
+                                }
+                                break;
+                            }
+                            rotated_english_language = english_language;
+                        }
+                    }
+                    if (index == (imposed_key.size()-1)) {
+                        index = 0;
+                    } else {
+                        index++;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    decrypted_message.shrink_to_fit();
+
+    std::cout << "Your decrypted result: ";
+    for (size_t i(0); i < decrypted_message.size(); i++) {
+        std::cout << decrypted_message[i];
+    }
+
+}
+
