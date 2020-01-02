ba42c490ad96ee9fe3450636e9224866a9eda3c5
Alexander Petrosyan <modernpacifist@gmail.com>
2020-01-02 03:22:26 +0300

    vim sessions added to .gitignore

--- src/encrypt_data.h
+++ src/encrypt_data.h
@@ -8,17 +8,18 @@ void encrypting_algorithm(std::string imposed_key, std::string imposed_message, 
     std::vector<char> rotated_english_language = english_language;
     
     std::string encrypted_message; 
-
+	
     char letter_for_encrypted_message;
     int index = 0;
     
-    for (int i = 0; i < imposed_message.length(); i++) {
+	for (int i = 0; i < imposed_message.length(); i++) {
         for (int j = 0; j < english_language.size(); j++) {
             if (imposed_message[i] == english_language[j]) {
                 for (int k = index; k < imposed_key.length();) {
                     for (int l = 0; l < rotated_english_language.size(); l++) {
                         if (imposed_key[k] == rotated_english_language[l]) {
-                            std::rotate(rotated_english_language.begin(), rotated_english_language.begin() + l, rotated_english_language.end());
+                            std::rotate(rotated_english_language.begin(), 
+                            rotated_english_language.begin() + l, rotated_english_language.end());
                             letter_for_encrypted_message = rotated_english_language[j];
                             encrypted_message.push_back(letter_for_encrypted_message);
                             rotated_english_language = english_language;
--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,26 @@
 int main () {
     std::vector<char> english_language = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
                                           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    //std::vector<char> *_ptr_english_language = &english_language;
     std::vector<char> *_ptr_english_language = &english_language;
-    
+	
     std::cout << "English is operatable" << std::endl;
     std::cout << "Available modes: -e | -d" << std::endl;
     std::string chosen_mode = recurcive_mode_input();
-    
+	
+    std::string(*_ptr_data_input)(std::string);
+    _ptr_data_input = &data_input;
+	
     if (chosen_mode == "encrypting") {
-        std::string (*_ptr_data_input)(std::string);
-        _ptr_data_input = &data_input;
-
-        std::string imposed_key = (*_ptr_data_input)("Input your key:");
+        std::string imposed_key = (*_ptr_data_input)("Input key:");
 
         std::string imposed_message = (*_ptr_data_input)("Input your message:"); 
-       
-        encrypting_algorithm(imposed_key, imposed_message, *_ptr_english_language);
+		
+        encrypting_algorithm(imposed_key, imposed_message, english_language);
 
         return EXIT_SUCCESS;
     } else if (chosen_mode == "decrypting") {
-        std::string (*_ptr_data_input)(std::string);
-        _ptr_data_input = &data_input;
-
-        std::string imposed_key = (*_ptr_data_input)("Input your prediction key:");
+        std::string imposed_key = (*_ptr_data_input)("Input key:");
         
         std::string imposed_message = (*_ptr_data_input)("Input your message:"); 
 
