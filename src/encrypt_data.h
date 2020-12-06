1e7bc134a1461e46a17153514fbec42fb253ff85
modernpacifist <modernpacifist@gmail.com>
2020-12-06 19:04:35 +0300

    fixed till first primitive working encryption version

--- src/encrypt_data.h
+++ src/encrypt_data.h
@@ -8,61 +8,50 @@ class Encryptor {
     private:
         std::string plain_text;
         std::string key;
-        std::string lang_choice;
         std::string cipher_text;
-		sdt::vector<char> alphabet;
 
 		// availabe languages
-		std::array<char, 26>english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+		std::vector<char>english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
 			'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     public:
-        void set_plain_text(std::string plain_text) { this -> plain_text = plain_text; }
-        std::string get_plain_text() { return plain_text; }
-
-        void set_key(std::string key) { this -> key = key; }
-        std::string get_key() { return key; }
-
-        void set_language(std::string language) { this -> language = language; }
-        std::string get_language() { return language; }
-
-        std::string encrypt(std::string, std::string, std::vector<char>);
+        void encrypt_data();
 
         std::string get_cipher_text() { return cipher_text; }
 
-        Encryptor(std::string, std::string, std::vector<char>);
+        Encryptor(std::string, std::string);
 };
 
 
-Encryptor::Encryptor(std::string plain_text, std::string key, std::vector<char> lang_choice) {
+Encryptor::Encryptor(std::string plain_text, std::string key) {
     this -> plain_text = plain_text;
     this -> key = key;
-    this -> alphabet = alphabet;
 }
 
 
-std::string Encryptor::encrypt(std::string imposed_key, std::string imposed_message, std::vector<char> english_language) {
-    std::vector<char> rotated_english_language = english_language;
-    
-    std::string encrypted_message; 
+void Encryptor::encrypt_data() {
+	std::string key = this -> key;
+	std::string plain_text = this -> plain_text;
+
+    std::vector<char> rotated_english_language = this->english_alphabet;
 
     char letter_for_encrypted_message;
     int index = 0;
 
-    for (int i = 0; i < imposed_message.length(); i++) {
-        for (int j = 0; j < english_language.size(); j++) {
-            if (imposed_message[i] == english_language[j]) {
-                for (int k = index; k < imposed_key.length();) {
+    for (int i = 0; i < plain_text.length(); i++) {
+        for (int j = 0; j < this->english_alphabet.size(); j++) {
+            if (plain_text[i] == this->english_alphabet[j]) {
+                for (int k = index; k < key.length();) {
                     for (int l = 0; l < rotated_english_language.size(); l++) {
-                        if (imposed_key[k] == rotated_english_language[l]) {
+                        if (key[k] == rotated_english_language[l]) {
                             std::rotate(rotated_english_language.begin(), 
                             rotated_english_language.begin() + l, rotated_english_language.end());
                             letter_for_encrypted_message = rotated_english_language[j];
-                            encrypted_message.push_back(letter_for_encrypted_message);
-                            rotated_english_language = english_language;
+                            this->cipher_text.push_back(letter_for_encrypted_message);
+                            rotated_english_language = this->english_alphabet;
                         }
                     }
-                    if (index == (imposed_key.length() - 1)) {
+                    if (index == (key.length() - 1)) {
                         index = 0;
                     } else {
                         index++;
@@ -72,5 +61,4 @@ std::string Encryptor::encrypt(std::string imposed_key, std::string imposed_mess
         }
     }
 
-    return encrypted_message;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,77 +6,21 @@
 #include <array>
 
 
-// TODO: function to parse command line arguments
-// std::array<char, 3> parse_command_line_args(int argc, char argv[]) {
-//     if (argc == 3) {
-//         // std::array<char, 3> a1 = {argv[0], argv[1], argv[2]};
-//         return {argv[0], argv[1], argv[2]};
-//     } else {
-//         std::cout << "usage <script> <message> <key>" << std::endl;
-//         std::cout << "type -h for help and usage examples" << std::endl;
-//         std::exit(EXIT_FAILURE);
-//     }
-// }
-
-
-const std::vector<char>english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
-	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
-
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cout << "usage <script> <message> <key>" << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
+	if (argc != 3) {
+		std::cout << "usage <script> <message> <key>" << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
 
+    std::string plain_text = argv[1];
+    std::string key = argv[2];
 
-    for (int i = 0; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
-    }
+	Encryptor encryptor(plain_text, key);
 
-    std::string plain_text = "fuckyou";
-    std::string key = "key";
+	encryptor.encrypt_data();
 
-	Encryptor encryptor(plain_text, key, english_alphabet);
-	encryptor.encrypt();
+	std::cout << encryptor.get_cipher_text() << std::endl;
 
 	std::exit(EXIT_SUCCESS);
 
 }
-
-
-// int main (int argc, char *argv[]) {
-//     if (argc != 4) {
-//         cout << "usage <script> <mode> <key> <message>" << endl;
-//         cout << "type -h for help and usage examples" << endl;
-//         return 1;
-//     }
-
-    // vector<char> english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    //                                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    // vector<char> *_ptr_english_language = &english_alphabet;
-
-//     string chosen_mode = argv[1];
-
-//     string(*_ptr_data_input)(string);
-//     _ptr_data_input = &data_input;
-
-//     if (chosen_mode == "encrypting") {
-//         string imposed_key = (*_ptr_data_input)("Input key:");
-
-//         string imposed_message = (*_ptr_data_input)("Input your message:");
-
-//         encrypting_algorithm(imposed_key, imposed_message, english_alphabet);
-
-//         return EXIT_SUCCESS;
-//     } else if (chosen_mode == "decrypting") {
-//         string imposed_key = (*_ptr_data_input)("Input key:");
-
-//         string imposed_message = (*_ptr_data_input)("Input your message:");
-
-//         decrypting_algorithm(imposed_key, imposed_message, *_ptr_english_language);
-
-//         return EXIT_SUCCESS;
-//     }
-
-// }
