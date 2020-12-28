699e9431aa4a86927aa88d77dee32842b83f4668
modernpacifist <modernpacifist@gmail.com>
2020-12-28 21:02:47 +0300

    tabulations

--- src/Lang.h
+++ src/Lang.h
@@ -11,14 +11,14 @@ class Lang {
     private:
         std::string lang_choice;
 
-		std::vector<char>english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        std::vector<char>english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-		std::vector<char>requested_language;
+        std::vector<char>requested_language;
 
     public:
         Lang(std::string);
 
-		std::vector<char> get_lang();
+        std::vector<char> get_lang();
 };
 
 #endif
