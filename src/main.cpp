d01f0bed3ffdd93be0836038f554a7d93a2d6f03
modernpacifist <modernpacifist@gmail.com>
2021-05-16 22:01:14 +0300

    removed external class/.gitignore/simplified code

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,32 @@
 #include "Encryptor.h"
 #include "Decryptor.h"
-#include "Lang.h"
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+std::vector<char>g_eng_lang {{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+                              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}};
+
 
 int main(int argc, char *argv[]) {
-	if (argc != 4) {
-		cout << "usage: <script> <message> <key> <language>" << endl;
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 3) {
+        cout << "usage: <script> <message> <key>" << endl;
+        exit(EXIT_FAILURE);
+    }
 
     string plain_text = argv[1];
     string key = argv[2];
-    string lang_choice = argv[3];
 
-    Lang lang(lang_choice);   
-    vector<char> chosen_language = lang.get_lang();
+    Encryptor encryptor(plain_text, key, g_eng_lang);
+    encryptor.encrypt_data();
+    string cipher_text = encryptor.get_cipher_text();
+    cout << "encrypted result: " << cipher_text << endl;
 
-	Encryptor encryptor(plain_text, key, chosen_language);
-	encryptor.encrypt_data();
-	string cipher_text = encryptor.get_cipher_text();
-	cout << "encrypted result: " << cipher_text << endl;
+    Decryptor decryptor(cipher_text, key, g_eng_lang);
+    decryptor.decrypt_data();
+    string plain_text1 = decryptor.get_plain_text();
+    cout << "decrypted result: " << plain_text << endl;
 
-	Decryptor decryptor(cipher_text, key, chosen_language);
-	decryptor.decrypt_data();
-	string plain_text1 = decryptor.get_plain_text();
-	cout << "decrypted result: " << plain_text << endl;
-
-	exit(EXIT_SUCCESS);
-
+    exit(EXIT_SUCCESS);
 }
